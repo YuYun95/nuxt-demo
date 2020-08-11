511f3996908704907240f1837c7f2d7e34b503aa
YuYun95 <2219946651@qq.com>
2020-08-12 00:54:58 +0800

    feat: asyncData

---
+++ components/Foo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Foo</h1>
+    此处会报错，因为这是非页面组件，asyncData方法不会执行，所以foo是未定义
+    <p>{{foo}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Foo",
+
+  async asyncData() {
+    return {
+      foo: 'bar'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <nuxt-link to="/">Home</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about"
+}
+</script>
+
+<style scoped>
+
+</style>
